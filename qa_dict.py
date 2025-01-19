ea060aa2fd6267e6c77139b31bdd2d79f190d725
Ahmad Soliman <ahmadadel92@gmail.com>
2025-01-20 01:35:08 +0200

    download qa if it doesn't exist

--- qa_dict.py
+++ qa_dict.py
@@ -1,5 +1,24 @@
 import json
+import os
+from google.cloud import storage
 
-with open("public/qa.json", "r", encoding="utf-8") as f:
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+
+# Replace with your bucket name and blob name
+bucket_name = "eslam-qa-netherlands"
+source_blob_name = "rendered_qa-mo3amalat.json"
+destination_file_name = "public/qa.json"
+
+if not os.path.exists(destination_file_name):
+    download_blob(bucket_name, source_blob_name, destination_file_name)
+
+with open(destination_file_name, "r", encoding="utf-8") as f:
     qa_data = json.load(f)
 qa_dict = {str(qa["id"]): qa for qa in qa_data}
