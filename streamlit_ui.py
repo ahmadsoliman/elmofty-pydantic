94b282de6a42fa2310dc8172a4bf7337065797da
Ahmad Soliman <ahmadadel92@gmail.com>
2025-01-20 19:30:51 +0200

    building it up 1

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -4,10 +4,34 @@ import nest_asyncio
 
 
 import streamlit as st
+import logfire
 from arabic_support import support_arabic_text
 
 # Support Arabic text alignment in all components
 support_arabic_text(components=["input", "markdown", "textinput"])
+
+
+# Import all the message part classes
+from pydantic_ai.messages import (
+    ModelMessage,
+    ModelRequest,
+    ModelResponse,
+    SystemPromptPart,
+    UserPromptPart,
+    TextPart,
+    ToolCallPart,
+    ToolReturnPart,
+    RetryPromptPart,
+    ModelMessagesTypeAdapter,
+)
+from pydantic_agent import pydantic_islam_agent, PydanticAIDeps, RAGToolTracker
+
+# Load environment variables
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from vertexai.preview.generative_models import GenerativeModel
 
 
 async def main():
