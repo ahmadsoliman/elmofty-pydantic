97872400ea740e85221f95e867f33273415aeea9
Ahmad Soliman <ahmadadel92@gmail.com>
2025-01-20 02:20:25 +0200

    fix

--- qa_dict.py
+++ qa_dict.py
@@ -5,7 +5,7 @@ from google.cloud import storage
 
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
-    storage_client = storage.Client()
+    storage_client = storage.Client(project="el-mofty")
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(source_blob_name)
     blob.download_to_filename(destination_file_name)
